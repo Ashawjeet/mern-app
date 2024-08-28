23d31d2ada94bce3336519ee954bd2517ee9faa9
ashawjeet <ashawjeetshinde2016@gmail.com>
2024-08-28 16:14:36 +0530

    ma

--- server/config/db.js
+++ server/config/db.js
@@ -15,27 +15,27 @@
 
 
 
+//28aug
+// const mongoose = require("mongoose");
 
-const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB connected...");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected...");
+//     // Test the database connection
+//     const connectionStatus = await mongoose.connection.db.admin().ping();
+//     console.log("MongoDB connection status:", connectionStatus);
+//   } catch (err) {
+//     console.error("Database connection error:", err.message);
+//     process.exit(1);
+//   }
+// };
 
-    // Test the database connection
-    const connectionStatus = await mongoose.connection.db.admin().ping();
-    console.log("MongoDB connection status:", connectionStatus);
-  } catch (err) {
-    console.error("Database connection error:", err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+// module.exports = connectDB;
 
 
 
@@ -56,3 +56,24 @@ module.exports = connectDB;
 
 // module.exports = connectDB;
  
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const mongoURI = process.env.MONGODB_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
+
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error("Database connection error:", err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
